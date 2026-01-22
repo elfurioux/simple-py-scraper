ed31ec5bee678fc6948c8d44e1b3ac527a0396f3
elfurioux <60098198+elfurioux@users.noreply.github.com>
2026-01-22 02:38:27 +0100

    feat: add resolveurl so the relative urls are complete

--- sps.py
+++ sps.py
@@ -2,6 +2,10 @@ import requests
 from html import parser as html
 from urllib import parse as urlt
 
+USE_HTTPS = True
+__PROTOCOL = "https" if USE_HTTPS else "http"
+__URLPREFIX = __PROTOCOL+"://"
+
 class URL:
     def __init__(self, url: str) -> None:
         self._url = url
@@ -16,6 +20,10 @@ class URL:
     def isrelative(self) -> bool:
         return self._url.startswith("/")
     
+    def reltohttp(self, urlbeginpart: str):
+        self._url = urlbeginpart.removesuffix("/")+self._url
+        assert self.ishttp()
+
     def isvalid(self):
         return self.ishttp() or self.isrelative()
 
@@ -27,7 +35,6 @@ class URL:
         return f"URL({l}'{self._url}')"
 
 class HTMLScraper(html.HTMLParser):
-
     def __init__(self, *, convert_charrefs = True):
         self.urllist: list[URL] = []
         super().__init__(convert_charrefs=convert_charrefs)
@@ -40,30 +47,41 @@ class HTMLScraper(html.HTMLParser):
                 u = URL(attr[1])
                 self.urllist.append(u)
 
+    def resolveurls(self, urlbeginpart: str):
+        i = 0
+        while i < len(self.urllist):
+            if self.urllist[i].isrelative():
+                self.urllist[i].reltohttp(urlbeginpart)
+            i += 1
+
     def printurls(self):
         for url in self.urllist:
             print(url)
 
-def htmlparse(srchtml: str):
-    scraper = HTMLScraper()
-
-    scraper.feed(srchtml)
-    scraper.printurls()
-
-
 def main(argv: list[str]) -> int:
     if (len(argv) < 1):
-        print("ERROR: please provide request url as argument: python sps.py <url>")
+        print("ERROR: please provide request host as argument: python sps.py <host>")
         return 1
 
-    _url = argv[0]
-    r = requests.get(url=_url)
+    host = argv[0]
+    hosturl = __URLPREFIX+host
+
+    if host.startswith("http") or host.find('/')!=-1:
+        print("ERROR: please provide just the hostname, not a url (example: 'google.com', 'en.wikipedia.org')")
+        return 1
+
+    scraper = HTMLScraper()
+    r = requests.get(url=hosturl)
 
     if r.status_code != requests.codes.ok:
         print(f"ERROR: Request to address \"{r.url}\" responded error code {r.status_code}")
         return 1
 
-    htmlparse(r.text)
+    scraper.feed(r.text)
+    scraper.printurls()
+    scraper.resolveurls(hosturl)
+    print("===")
+    scraper.printurls()
 
     return 0
 
